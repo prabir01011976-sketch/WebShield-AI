4693176f8a58435d30e548b3e634dd5204468cee
Raj Ghosh <prabir01011976@gmail.com>
2026-08-01 20:17:34 +0530

    Added HTTP website scanner

--- backend/app/scanner/scan.py
+++ backend/app/scanner/scan.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter
 from app.models.scan import ScanRequest
+import requests
 
 router = APIRouter()
 
 @router.post("/scan")
 def scan(data: ScanRequest):
-    return {
-        "message": "Scan Started",
-        "url": data.url
-    }
+    try:
+        response = requests.get(data.url, timeout=10)
+
+        return {
+            "message": "Scan Completed",
+            "url": data.url,
+            "status_code": response.status_code,
+            "server": response.headers.get("Server", "Unknown")
+        }
+
+    except requests.exceptions.RequestException as e:
+        return {
+            "message": "Website Unreachable",
+            "url": data.url,
+            "error": str(e)
+        }
